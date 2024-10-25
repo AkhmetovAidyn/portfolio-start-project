34876efe2e8268682ac64483edc07c51913f3ba1
Akhmetov Aidyn <ahmetovv_aidyn@mail.ru>
2024-10-25 09:16:57 +0500

    change photo

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import styled from "styled-components";
+import photo from '../../../assets/images/photo.jpg'
 
 export const Main = () => {
     return (
         <div>
-
+            <div>
+                <span>Hi There</span>
+                <h2>I am Svetlana Dyablo</h2>
+                <h1>A Web Developer.</h1>
+                <Photo src={photo} alt=""/>
+            </div>
         </div>
     );
 };
 
+const Photo = styled.img`
+    width: 350px;
+    height: 430px;
+`
