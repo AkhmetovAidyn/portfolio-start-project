33d086fdef49834432977d1b0b2fea2ce481effb
Akhmetov Aidyn <ahmetovv_aidyn@mail.ru>
2024-10-25 14:44:59 +0500

    add main component

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -1,21 +1,30 @@
 import React from 'react';
 import styled from "styled-components";
 import photo from '../../../assets/images/photo.jpg'
+import {FlexWrapper} from "../../../conmponents/FlexWrapper";
 
 export const Main = () => {
     return (
-        <div>
-            <div>
-                <span>Hi There</span>
-                <h2>I am Svetlana Dyablo</h2>
-                <h1>A Web Developer.</h1>
-                <Photo src={photo} alt=""/>
-            </div>
-        </div>
+        <StyledMain>
+            <FlexWrapper align={"center"} justify={"space-around"}>
+                <div>
+                    <span>Hi There</span>
+                    <h2>I am Svetlana Dyablo</h2>
+                    <h1>A Web Developer.</h1>
+                </div>
+
+                    <Photo src={photo} alt=""/>
+            </FlexWrapper>
+        </StyledMain>
     );
 };
+
+const StyledMain = styled.div`
+    min-height: 100vh;
+`
 
 const Photo = styled.img`
     width: 350px;
     height: 430px;
+    object-fit: cover;
 `
