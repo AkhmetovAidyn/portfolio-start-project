625c0c08d639b8ffdf27d235ae0da8c09d9546da
Akhmetov Aidyn <ahmetovv_aidyn@mail.ru>
2024-11-09 15:54:56 +0500

    update Global and TabMenu

--- src/layout/sections/works/tabMenu/TabMenu.tsx
+++ src/layout/sections/works/tabMenu/TabMenu.tsx
@@ -1,26 +1,50 @@
 import React from 'react';
 import styled from "styled-components";
+import {theme} from "../../../../syles/Theme";
 
 
 export const TabMenu = (props: {menuItems: Array<string>}) => {
     return (
-        <StyledMenu>
+        <StyledTabMenu>
             <ul>
                 {props.menuItems.map((item, index) => {
-                    return <li key={index}>
-                        <a href="">{item}</a>
-                    </li>
+                    return <ListItem key={index}>
+                        <Link href="">{item}</Link>
+                    </ListItem>
                 })}
             </ul>
-        </StyledMenu>
+        </StyledTabMenu>
     );
 };
 
 
-const StyledMenu = styled.nav`
+const StyledTabMenu = styled.nav`
     ul {
         display: flex;
         gap: 30px;
         justify-content: center;
     }
-` 
+`
+
+const ListItem = styled.li `
+    position: relative;
+`
+
+const Link = styled.a`
+    font-weight: 400;
+    font-size: 14px;
+    letter-spacing: 0.07em;
+    text-transform: uppercase;
+    
+    &:before {
+        content: '';
+        display: inline-block;
+        height: 10px;
+        background-color: ${theme.colors.accent};
+        
+        position: absolute;
+        bottom: 0;
+        left: -10px;
+        right: -10px;
+    }
+`
