90df1be08b4de6f10e2ce6b72ad239d946b8af61
Akhmetov Aidyn <ahmetovv_aidyn@mail.ru>
2024-11-24 17:35:56 +0500

    refactoring testimony

--- src/layout/sections/testimony/Testimony.tsx
+++ src/layout/sections/testimony/Testimony.tsx
@@ -4,11 +4,10 @@ import {SectionTitle} from "../../../conmponents/SectionTitle";
 import {Icon} from "../../../conmponents/icon/Icon";
 import {Slider} from "../../../conmponents/slider/Slider";
 import {FlexWrapper} from "../../../conmponents/FlexWrapper";
-// import {IconWrapper} from "../skills/skill/Skill";
 import {Container} from "../../../conmponents/Container";
 import {S} from '../skills/Skills_Styles'
 
-export const Testimony = () => {
+export const Testimony: React.FC = () => {
     return (
         <StyledTestimony>
             <Container>
