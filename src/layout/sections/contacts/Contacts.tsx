e05b9132c1135679b199737926620bc377e490ac
Akhmetov Aidyn <ahmetovv_aidyn@mail.ru>
2024-12-05 08:42:35 +0500

    add sendEmail function

--- src/layout/sections/contacts/Contacts.tsx
+++ src/layout/sections/contacts/Contacts.tsx
@@ -1,18 +1,43 @@
-import React from 'react';
+import React, {ElementRef, useRef} from 'react';
 import {SectionTitle} from "../../../conmponents/SectionTitle";
 import {Button} from "../../../conmponents/Button";
 import {Container} from "../../../conmponents/Container";
-import {S} from './Contacts_Styles'
+import {S} from './Contacts_Styles';
+import emailjs from '@emailjs/browser';
 
 export const Contact: React.FC = () => {
+
+    const form = useRef<ElementRef<'form'>>(null);
+
+    const sendEmail = (e: any) => {
+        e.preventDefault();
+
+        if(!form.current) return
+
+        emailjs
+            .sendForm('service_xvmos6k', 'template_3myjwy9', form.current, {
+                publicKey: 'Ae76Q-bsR0Vc2nqgf',
+            })
+            .then(
+                () => {
+                    console.log('SUCCESS!');
+                },
+                (error) => {
+                    console.log('FAILED...', error.text);
+                },
+            );
+        e.target.reset()
+    };
+
     return (
         <S.Contacts id={"contact"}>
             <Container>
                 <SectionTitle>Contact</SectionTitle>
-                <S.Form>
-                    <S.Field placeholder={"name"}/>
-                    <S.Field placeholder={"subject"}/>
-                    <S.Field placeholder={"message"} as={"textarea"}/>
+                <S.Form ref={form} onSubmit={sendEmail}>
+                    <S.Field required placeholder={"name"} name={"user_name"}/>
+                    <S.Field required placeholder={"email"} name={"email"}/>
+                    <S.Field required placeholder={"subject"} name={"subject"}/>
+                    <S.Field required placeholder={"message"} as={"textarea"} name={"message"}/>
                     <Button type={"submit"}>Send message</Button>
                 </S.Form>
             </Container>
